eda4853cbe1b04ab8526ed6b1c6c98bbd574e0fd
umidkhan <gameblogger924@gmail.com>
2022-07-06 15:49:48 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let your_data = confirm( "O'zingiz haqida ma`lumot beryapsiz! Davom etish uchun OK tugmasini bosing " )
+let a = prompt( 'Ismingizni kiriting' )
+let b = +prompt( 'Yoshingizni kiriting' )
+let c = prompt( 'Familiyangizni kiriting' )
+let d = prompt( 'Mashinangiz bormi?' )
+let f = prompt( 'Kelajakda kim bo`lmoqchisiz?' )
+let g = prompt( 'Bosh vaqtlaringizda nima bilan shug`ullanasiz?' )
+let h = prompt( 'Nimalarga qiziqasiz?' )
+let save = confirm( 'Rahmat. Ma`lumotlaringiz saqlandi' )
+
+console.log(a, b, c, d, f, g, h);
